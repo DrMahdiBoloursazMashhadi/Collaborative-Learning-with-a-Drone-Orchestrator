45983b44b4ad21f1a8d5efe217d806f4aa75c0b2
DrMahdiBoloursazMashhadi <121172212+DrMahdiBoloursazMashhadi@users.noreply.github.com>
2023-06-16 20:15:01 +0100

    Delete Trajectory Optimization directory

--- Trajectory Optimization/CalcError.m
+++
@@ -1,4 +0,0 @@
-function error = CalcError(theta, B, N0,p_k,cc,pi,fc,H,eta1,puav,coordinate,alpha)
-    sigma =  (cc/(4*pi*fc))^2*(eta1)*(H^2+(coordinate(1)-puav(1))^2+(coordinate(2)-puav(2))^2)^(alpha/2);
-    error = (1-exp(-(theta*B*N0)/(sigma*p_k)));
-end
